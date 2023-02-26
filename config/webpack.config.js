f263cb55972298abc17c9350a09b8e5035c3b6dd
wujimalayo <597605131@qq.com>
2023-02-26 18:17:10 +0800

    postcss单独配置

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -27,8 +27,9 @@ const ForkTsCheckerWebpackPlugin =
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 const createEnvironmentHash = require("./webpack/persistentCache/createEnvironmentHash");
-
-// const px2viewport = require("postcss-px-to-viewport");
+const postCssConfig = require("../postcss.config");
+const postcsspx2viewportConfig =
+  postCssConfig.plugins["postcss-px-to-viewport"];
 
 // Source maps are resource heavy and can cause out of memory issue for large source files.
 const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== "false";
@@ -150,27 +151,7 @@ module.exports = function (webpackEnv) {
                   // so that it honors browserslist config in package.json
                   // which in turn let's users customize the target behavior as per their needs.
                   "postcss-normalize",
-                  [
-                    "postcss-px-to-viewport",
-                    {
-                      unitToConvert: "px", //需要转换的单位，默认为"px"
-                      viewportWidth: 350, // 视窗的宽度，对应的是我们设计稿的宽度
-                      viewportHeight: 1334, //视窗的高度，根据375设备的宽度来指定，一般指定667，也可以不配置
-                      unitPrecision: 13, // 指定`px`转换为视窗单位值的小数位数（很多时候无法整除）
-                      propList: ["*"], // 能转化为vw的属性列表
-                      viewportUnit: "vmin", // 指定需要转换成的视窗单位，建议使用vw
-                      fontViewportUnit: "vmin", //字体使用的视口单位
-                      selectorBlackList: [".ignore-", ".hairlines"], //指定不转换为视窗单位的类，可以自定义，可以无限添加,建议定义一至两个通用的类名
-                      minPixelValue: 1, // 小于或等于`1px`不转换为视窗单位，你也可以设置为你想要的值
-                      mediaQuery: false, // 允许在媒体查询中转换`px`
-                      replace: true, //是否直接更换属性值，而不添加备用属性
-                      exclude: [/.yarn/, /node_modules/], //忽略某些文件夹下的文件或特定文件，例如 'node_modules' 下的文件
-                      include: [/src/],
-                      landscape: false, //是否添加根据 landscapeWidth 生成的媒体查询条件 @media (orientation: landscape)
-                      landscapeUnit: "vmin", //横屏时使用的单位
-                      landscapeWidth: 1134, //横屏时使用的视口宽度
-                    },
-                  ],
+                  ["postcss-px-to-viewport", postcsspx2viewportConfig],
                 ]
               : [
                   "tailwindcss",
